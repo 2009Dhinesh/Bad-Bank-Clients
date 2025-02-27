31ee06cbd324384c95a0803ce451bec8f7b42ccc
2009Dhinesh <dhineshwaran1712@gmail.com>
2025-02-27 14:25:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Bank from './BadMain';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
