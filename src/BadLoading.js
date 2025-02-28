62032bbfc4457c984457f55de53402c2dc768101
2009Dhinesh <dhineshwaran1712@gmail.com>
2025-02-28 13:34:25 +0530

    Add files via upload

---
+++ src/BadLoading.js
@@ -0,0 +1,23 @@
+import Logo from './images/Atom Loader.gif'
+export default function Loading (){
+
+
+
+    return(<>
+    {/* <div style={{
+        position:'absolute',
+        top:'40%',
+
+    }}> */}
+
+    <img src={Logo} alt="" style={{
+        width:"100px",
+        position:'absolute',
+        top:'40%',
+
+    }}/>
+    {/* <span>Dhisha Bank</span> */}
+    {/* </div> */}
+
+    </>)
+}
