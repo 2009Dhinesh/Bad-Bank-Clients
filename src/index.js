655ab32e10021cd97eca95ce004902840df8938b
2009Dhinesh <dhineshwaran1712@gmail.com>
2025-02-28 13:26:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import './style.css';
-// import App from './App';
-// import Ap from './cA.js';
-// import ATM from './Atm.js';
-// import Bank from './bankIndex';
-// import Fatch from './fetch.js';
-// import CA5 from './cA-5.1';
-// import CAS5 from './cAS5';
-// import Tic from './Tic';
-// import CA_Main from './C_A_6.1_Main';
-// import CA_Main from './CA_6';
-// import Boostrap from './Boostrap';
-// import MainBooks from './MainBooks';
-// import CAMain from './CAMain_7.1';
-// import CAMain from './Blog';
-// import CAMain from './linkedPage';
-// import CAMain from './linkedPage02';
-// import CAMain from './CA_7.2';
-// import CAMain from './CAS07Main';
-// import CAMain from './PorMain';
-import CAMain from './BadMain';
-// import Shopping from './ShoppingCart/index'
+import Main from './BadMain';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    {/* <Ap/> */}
-    {/* <ATM /> */}
-    {/* <Fatch /> */}
-    {/* <CA5 /> */}
-    {/* <CAS5 /> */}
-    {/* <Tic /> */}
-    {/* <CA_Main /> */}
-    {/* <Bank /> */}
-    {/* <Boostrap /> */}
-    {/* <MainBooks /> */}
-    <CAMain />
-    {/* <Shopping /> */}
+    <Main />
   </React.StrictMode>
 );
 
