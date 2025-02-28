dc0130396e8e790ba8af0cb7239da07b0ea21507
2009Dhinesh <dhineshwaran1712@gmail.com>
2025-02-28 15:24:20 +0530

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,7 +1,5 @@
 import { Button } from 'react-bootstrap';
-// import { useContext} from 'react';
 import { useState } from 'react';
-// import UserContext  from './context';
 import deps from './register.jpg'
 import axios from 'axios';
 import { FaBuildingColumns } from "react-icons/fa6";
@@ -10,7 +8,6 @@ import SubMain from './SubBadMain'
 
 export default function Register(){
 
-    // const users = useContext(UserContext);
 
     const [userId , setUserid] = useState('');
     const [name , setName] = useState('');
@@ -20,28 +17,12 @@ export default function Register(){
 
 const handleSubmit=(e)=>{
     e.preventDefault();
-    // users.users.push({name:name,email:email,password:password,amount:1000})
-
-    // connect to front to back end
-
     let item = {userid:userId,name:name,email:email,password:password,amount:1000};
-    axios.post('http://localhost:3002/create' , item);
-    // async function duplicate(){
-    //     let response = await (axios.post('http://localhost:3002/create'));
-    // let result =response.data;
-    // console.log(response)
-    // console.log(result)
-    // }
-    // duplicate();
-    
-    // (item === result.userid) ? console.log("duplicate") : console.log("succ")
+    axios.post('https://server-90ct.onrender.com/create' , item);
     document.getElementById('display').style.display = "block";
     setResult(` Your Account create in Successfully....`);
-    console.log(item.userid)
 
 
-
-    // console.log(users)
 }
 
     return(<>
@@ -67,7 +48,7 @@ const handleSubmit=(e)=>{
         <input type="password" onChange={(e)=>setPassword(e.target.value)}/> <br /><br />
         
         <Button type='submit' >Submit</Button>
-        {/* onClick={()=>alert('register successfully.... and You pay inizial amount is 1000')}  */}
+
     </form>
 
     <p className='inizial-amount'><FaBuildingColumns style={{marginRight:'2%'}}/>You are pay inizial amount is 1000</p>
