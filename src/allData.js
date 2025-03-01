5492a480a56fb872d9cedc582d56592bbd1e9bb2
2009Dhinesh <dhineshwaran1712@gmail.com>
2025-03-01 10:36:23 +0530

    Update allData.js

--- src/allData.js
+++ src/allData.js
@@ -93,7 +93,6 @@ console.log(products)
 
     {editId && (
             <div>
-            <h2>Edit Data</h2>
             <input type="text" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
             <input type="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
             <input type="password" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
