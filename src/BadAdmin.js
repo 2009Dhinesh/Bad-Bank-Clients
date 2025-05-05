8bf509183473d8fc2463503626d3747ac70fc657
Dhinesh Waran P <dhineshwaran1712@gmail.com>
2025-05-05 11:08:01 +0530

    Update BadAdmin.js

--- src/BadAdmin.js
+++ src/BadAdmin.js
@@ -1,25 +1,35 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { NavDropdown } from 'react-bootstrap';
 
 function BasicExample() {
-  return (<>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container style={{position:"absolute" , top:'-500%' , left :'35%' ,fontSize:'20px'}}>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#register">Register</Nav.Link>
-            <Nav.Link href="#deposit">Deposit</Nav.Link>
-            <Nav.Link href="#cashback">Withdrawal</Nav.Link>
-            <Nav.Link href="#allData">All Data</Nav.Link>            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">My Brand</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#register">Register</Nav.Link>
+              <Nav.Link href="#deposit">Deposit</Nav.Link>
+              <Nav.Link href="#cashback">Withdrawal</Nav.Link>
+              <Nav.Link href="#allData">All Data</Nav.Link>
 
-</>);
+              {/* Dropdown for responsive layout */}
+              <NavDropdown title="More" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something else</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default BasicExample;
