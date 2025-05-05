73405b000a495ce29b6378f65efdc64612908e8f
Dhinesh Waran P <dhineshwaran1712@gmail.com>
2025-05-05 11:30:54 +0530

    Update BadAdmin.js

--- src/BadAdmin.js
+++ src/BadAdmin.js
@@ -3,17 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function BasicExample() {
-return (<> <Navbar expand="lg" className="bg-body-tertiary">
-\<Container style={{position:"absolute" , top:'-500%' , left :'35%' ,fontSize:'20px'}}>
-\<Navbar.Collapse id="basic-navbar-nav"> <Nav className="me-auto">
-\<Nav.Link href="#home">Home\</Nav.Link>
-\<Nav.Link href="#register">Register\</Nav.Link>
-\<Nav.Link href="#deposit">Deposit\</Nav.Link>
-\<Nav.Link href="#cashback">Withdrawal\</Nav.Link>
-\<Nav.Link href="#allData">All Data\</Nav.Link> </Nav>
-\</Navbar.Collapse> </Container> </Navbar>
+return (<> 
+<Navbar expand="lg" className="bg-body-tertiary">
+<Container style={{position:"absolute" , top:'-500%' , left :'35%' ,fontSize:'20px'}}>
+<Navbar.Collapse id="basic-navbar-nav"> <Nav className="me-auto">
+<Nav.Link href="#home">Home</Nav.Link>
+<Nav.Link href="#register">Register</Nav.Link>
+<Nav.Link href="#deposit">Deposit</Nav.Link>
+<Nav.Link href="#cashback">Withdrawal</Nav.Link>
+<Nav.Link href="#allData">All Data</Nav.Link> </Nav>
+</Navbar.Collapse> </Container> </Navbar>
 
-\</>);
+</>);
 }
 
 export default BasicExample;   
